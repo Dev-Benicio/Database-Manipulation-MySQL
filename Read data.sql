5ec6199eb279aa9da51aac0440c347f133917a2f
Dev-Benicio <161643883+Dev-Benicio@users.noreply.github.com>
2024-10-27 23:48:03 -0300

    Update Read data.sql

--- Read data.sql
+++ Read data.sql
@@ -6,38 +6,41 @@
     
 
 -- Buscando os dados da Tabela Editora
-	select * from Editora;
+    select * from Editora;
     select * from Editora where idEditora=1;
     
 -- Buscando dados da tabela Autor
-	select * from Autor;
+    select * from Autor;
     select * from Autor where idAutor=1;
     
 -- Buscando dados da Tabela Livro
-	select * from Livro;
+    select * from Livro;
     select * from Livro where idLivro=1;
     
  -- Buscando dados na Tabela Usuario
-	select * from Usuario;
+    select * from Usuario;
     select * from Usuario where idUsuario=1;
     
 -- Buscando dados da tabela Emprestimo
-	select * from Emprestimo;
+    select * from Emprestimo;
     select * from Emprestimo where idEmprestimo=2;
     
 -- Busca Livro do Autor junto com editora que publicou o Livro
-	select l.idLivro, l.titulo, l.isbn,
+-- Utilização do Inner Join, fazendo buscas do Dados do livro, autor e Editora
+
+    select l.idLivro, l.titulo, l.isbn,
     a.nome_do_autor, e.Editora_nome
     from Livro l
     inner join Autor a on l.idlivro = a.IdAutor
     inner join editora e on e.idEditora = a.idAutor;
 
+
 -- --------------------- BONUS -----------------------------
 
 -- Chama os emprestimos ativos de qual livro e para qual usuário.
-	 select em.idEmprestimo, l.titulo, u.nome, em.situacao from Livro l
+-- Utilização do Inner Join, faz uma busca dos livros reservados
+	 
+         select em.idEmprestimo, l.titulo, u.nome, em.situacao from Livro l
 	 inner join emprestimo em on l.idLivro = em.idEmprestimo
 	 inner join usuario u on u.idUsuario = em.idEmprestimo
 	 where em.situacao="Reservado";
-    
-    
